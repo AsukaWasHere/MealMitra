ab67eeb80ffb5e059eb7c998d9691959304e3cc6
Aryan Vats <aryanvats0212@gmail.com>
2025-10-25 15:06:32 +0530

    Delete src directory

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getStatus } = require("../controllers/testController");
-
-router.get("/", getStatus);
-
-module.exports = router;
